47043a4c5dd2fc2a705ac41afef746bb7224ce9b
Gayathriperiyasamy <gayathriperiyasamy34@gmail.com>
2025-08-07 12:09:46 +0530

    3rd data

---
+++ muthayammal/src/main/java/gitconnection/string.java
@@ -0,0 +1,16 @@
+package gitconnection;
+
+public class string {
+
+	public static void main(String[] args) {
+		String f1="GAYATHRI";
+		String f2="GAYATHRI";
+		System.out.println(f1==f2);
+		String n1=new String("sharvin");
+		String n2=new String("sharvin");
+		System.out.println(f1==f2);
+		
+
+	}
+
+}
